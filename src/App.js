bb0d137b7f35fabe0fa2d2ea8ef1bfa8e366f88e
Zaiyr <zaiyr.00@gmail.com>
2019-08-17 14:54:08 +0600

    qr functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Qr_code from './components/Qr_code.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <Qr_code
+
+      />
+
     </div>
   );
 }
